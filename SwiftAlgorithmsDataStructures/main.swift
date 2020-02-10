89331837491c0a3f5d5c7cf2ad0a217f8a66beb6
diego <espinosa9mx@gmail.com>
2020-02-10 15:35:08 -0800

    Initial Commit

---
+++ SwiftAlgorithmsDataStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgorithmsDataStructures
+//
+//  Created by Diego Espinosa on 2020-02-10.
+//  Copyright © 2020 Diego Espinosa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
